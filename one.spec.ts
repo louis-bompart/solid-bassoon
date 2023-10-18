05370131110437d627a8edd705de14d257bb5afe
Louis Bompart <lbompart@coveo.com>
2023-10-17 22:11:26 -0400

    simplify

--- one.spec.ts
+++ one.spec.ts
@@ -1,8 +1,5 @@
 describe("My First Test", () => {
-  it("will fail the first time", () => {
-    cy.exec("cat foo", { failOnNonZeroExit: false })
-      .its("code")
-      .should("eq", 0);
-    cy.exec("touch foo");
+  it("will always fail", () => {
+    expect(true).to.equal(false);
   });
 });
